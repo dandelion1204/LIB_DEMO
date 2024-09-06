b6b05b8939dbbae686677071c886fb71835f2506
Ting <dandelion1204@gmail.com>
2024-09-06 17:17:38 +0800

    migrate

--- LIB_DEMO/settings.py
+++ LIB_DEMO/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'books',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,14 @@ WSGI_APPLICATION = 'LIB_DEMO.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'booksdb',
+        'USER': 'django-admin',
+        'PASSWORD': 'Django0905',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
--- books/models.py
+++ books/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Books(models.Model):
+    name = models.CharField(max_length=50, blank=False, default='')
+    editor = models.CharField(max_length=50, blank=False, default='')
+    publish = models.CharField(max_length=50, blank=False, default='')
+    category = models.CharField(max_length=50, blank=False, default='')
+
+    def __str__(self) -> str:
+        return self.name
+    
+    class Meta:
+        ordering = ('id',)
